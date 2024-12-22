111af37da6fa7da1d99d74fcea13fff9f9e9cbad
JacekJochemczyk <j.jochemczyk@gmail.com>
2024-12-22 12:24:07 +0100

    Cos do przodu

---
+++ Biblioteka/Data/AplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenIddict.EntityFrameworkCore.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Konfiguracja OpenIddict w bazie danych
+        builder.UseOpenIddict();
+    }
+}
--- Biblioteka/Program.cs
+++ Biblioteka/Program.cs
@@ -21,6 +21,43 @@ builder.Services.AddHttpClient();
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    // Integracja z OpenIddict
+    options.UseOpenIddict();
+});
+
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        // U¿ycie Entity Framework Core dla OpenIddict
+        options.UseEntityFrameworkCore()
+               .UseDbContext<ApplicationDbContext>();
+    })
+    .AddServer(options =>
+    {
+        options.AllowAuthorizationCodeFlow(); // U¿ywamy Authorization Code Flow
+        options.SetTokenEndpointUris("/connect/token");
+        options.SetAuthorizationEndpointUris("/connect/authorize");
+
+        // U¿ycie certyfikatu deweloperskiego do szyfrowania
+        options.AddDevelopmentEncryptionCertificate(); // Dodanie certyfikatu deweloperskiego
+
+        // Dodanie klucza do podpisywania tokenów
+        options.AddEphemeralSigningKey(); // Tymczasowy klucz do podpisywania tokenów (do testów)
+
+        options.UseAspNetCore()
+               .EnableAuthorizationEndpointPassthrough()
+               .EnableTokenEndpointPassthrough();
+    })
+    .AddValidation(options =>
+    {
+        // Mo¿esz dodaæ opcje walidacji, jeœli masz zamiar walidowaæ tokeny
+    });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -40,8 +77,34 @@ app.UseRouting();
 app.UseAuthentication(); // Dodanie obs³ugi uwierzytelniania
 app.UseAuthorization(); // Dodanie obs³ugi autoryzacji
 
+app.UseAuthentication(); // Obs³uga uwierzytelniania
+app.UseAuthorization();  // Obs³uga autoryzacji
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var serviceProvider = scope.ServiceProvider;
+
+var applicationManager = serviceProvider.GetRequiredService<OpenIddict.Abstractions.IOpenIddictApplicationManager>();
+
+if (await applicationManager.FindByClientIdAsync("blazor-server") == null)
+{
+    await applicationManager.CreateAsync(new OpenIddict.Abstractions.OpenIddictApplicationDescriptor
+    {
+        ClientId = "blazor-server",
+        RedirectUris = { new Uri("https://localhost:5001/signin-oidc") },
+        Permissions =
+        {
+            OpenIddict.Abstractions.OpenIddictConstants.Permissions.Endpoints.Authorization,
+            OpenIddict.Abstractions.OpenIddictConstants.Permissions.Endpoints.Token,
+            OpenIddict.Abstractions.OpenIddictConstants.Permissions.GrantTypes.AuthorizationCode,
+            OpenIddict.Abstractions.OpenIddictConstants.Permissions.ResponseTypes.Code,
+            OpenIddict.Abstractions.OpenIddictConstants.Permissions.Scopes.Profile
+        }
+    });
+}
+
 
 app.Run();
