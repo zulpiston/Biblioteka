e6e17f9b50ab1d37d6b9831e23f9f9ea7cc3a7ab
JacekJochemczyk <j.jochemczyk@gmail.com>
2024-12-22 09:17:33 +0100

    Revert "openiddict - proba 2"
    This reverts commit 3596f192a6f25974bf049f0bf5d122fcd6ae6e37.

--- Biblioteka/Program.cs
+++ Biblioteka/Program.cs
@@ -1,100 +1,33 @@
-using Biblioteka.Authentication;
 using Biblioteka.Data;
-
+using Biblioteka.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using OpenIddict.Abstractions;
-using OpenIddict.Core;
-using OpenIddict.EntityFrameworkCore.Models;
-using static OpenIddict.Abstractions.OpenIddictConstants;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Dodanie konfiguracji baz danych
-builder.Services.AddDbContext<LibraryContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDbContext<AuthDbContext>(options =>
+// Add services to the container.
+builder.Services.AddHttpClient("BooksApi", client =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AuthConnection"));
-    options.UseOpenIddict();
+    client.BaseAddress = new Uri("https://localhost:7164/"); 
 });
-
-// Rejestracja ASP.NET Identity
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<AuthDbContext>()
-    .AddDefaultTokenProviders();
-
-// Konfiguracja OpenIddict
-builder.Services.AddOpenIddict()
-    .AddCore(options =>
-    {
-        options.UseEntityFrameworkCore()
-               .UseDbContext<AuthDbContext>();
-    })
-    .AddServer(options =>
-    {
-        options.AllowAuthorizationCodeFlow()
-               .RequireProofKeyForCodeExchange()
-               .AllowRefreshTokenFlow();
-
-        options.SetTokenEndpointUris("/connect/token")
-               .SetAuthorizationEndpointUris("/connect/authorize");
-
-        options.AddDevelopmentEncryptionCertificate()
-               .AddDevelopmentSigningCertificate();
-
-        options.UseAspNetCore()
-               .EnableTokenEndpointPassthrough()
-               .EnableAuthorizationEndpointPassthrough();
-    })
-    .AddValidation(options =>
-    {
-        options.UseLocalServer();
-        options.UseAspNetCore();
-    });
-
-// Dodanie pozosta³ych us³ug
 builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
 
+
+builder.Services.AddDbContext<LibraryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Migracja bazy danych i inicjalizacja OpenIddict
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
-    dbContext.Database.Migrate();
-
-    var manager = scope.ServiceProvider.GetRequiredService<OpenIddictApplicationManager<OpenIddictEntityFrameworkCoreApplication>>();
-    if (await manager.FindByClientIdAsync("your-client-id") == null)
-    {
-        await manager.CreateAsync(new OpenIddictApplicationDescriptor
-        {
-            ClientId = "your-client-id",
-            ClientSecret = "your-client-secret",
-            DisplayName = "Your Client Application",
-            RedirectUris = { new Uri("https://localhost:5001/signin-oidc") },
-            Permissions =
-            {
-                Permissions.Endpoints.Authorization,
-                Permissions.Endpoints.Token,
-                Permissions.GrantTypes.AuthorizationCode,
-                Permissions.ResponseTypes.Code,
-                Permissions.Scopes.Email,
-                Permissions.Scopes.Profile
-            }
-        });
-    }
-}
-
-// Konfiguracja middleware
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -104,13 +37,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseAuthentication(); // Dodanie obs³ugi uwierzytelniania
+app.UseAuthorization(); // Dodanie obs³ugi autoryzacji
 
-app.MapControllers();
 app.MapBlazorHub();
-
 app.MapFallbackToPage("/_Host");
-
+app.MapControllers();
 
 app.Run();
